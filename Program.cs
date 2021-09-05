6b0c2fdcb60cedfd807f6531ee4eda713bed7984
Atanasov <giorgioatanasov34@yahoo.com>
2021-09-05 14:13:32 -0700

    initial commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GameServer
+{
+    class Program
+    {
+        static void Main(string[] args) {
+
+            Console.Title = "Game Server";
+            Server.Start(50, 5345);
+            Console.ReadKey();
+        }
+    }
+}
